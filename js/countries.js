5b46676a737e492a0d88eefd88996975ff3002a9
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-11-15 16:13:02 +0600

    added country flag dynamically

--- js/countries.js
+++ js/countries.js
@@ -1,10 +1,26 @@
 const loadcountries = () => {
     fetch('https://restcountries.com/v2/all')
         .then(res => res.json())
-        .then(data = displayCountries(data));
+        .then(data => displayCountries(data));
 }
 
 const displayCountries = (countries) => {
-    console.log(data);
+
+    const countriesHTML = countries.map(country => getCountryHTML(country));
+
+    const container = document.getElementById('countries');
+
+    container.innerHTML = countriesHTML.join(' ');
 }
+const getCountryHTML = country => {
+    return `
+    <div>
+        <h3> ${country.name} </h3>
+        <img src="${country.flags.png}"></img>
+    </div>
+    `
+}
+
+
+
 loadcountries();
